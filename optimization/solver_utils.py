cc1f2f9a0e28f48f7d6bf876387be8c34502fbc8
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-01-29 22:27:41 +0800

    Update solver_utils.py

--- optimization/solver_utils.py
+++ optimization/solver_utils.py
@@ -1,1 +1,16 @@
+"""
+solver_utils.py
+Helper methods for solver selection, logging, or advanced debug.
+"""
+
+import pyomo.environ as pyo
+
+def get_solver(solver_name='ipopt'):
+    """
+    Return a configured solver instance.
+    """
+    solver = pyo.SolverFactory(solver_name)
+    # Could configure solver options here
+    # solver.options['tol'] = 1e-8
+    return solver
 
